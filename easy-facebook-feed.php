ba820dbcd535e119d21de4a0d919dc67bf82422d
timwass <timwass@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-21 19:23:19 +0000

    added widget support
    git-svn-id: https://plugins.svn.wordpress.org/easy-facebook-feed/tags/0.5@1203401 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ easy-facebook-feed.php
@@ -0,0 +1,112 @@
+<?php
+/**
+* Plugin Name: Easy Facebook Feed
+* Plugin URI: http://stormware.nl/
+* Description: Easy Facebook Feed shows your facebook feed on a wordpress page in a easy way
+* Version: 0.5
+* Author: Tim Wassenburg
+* Author URI: http://stormware.nl/
+* License: GPLv2 or later
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+// include facebook post functions
+include_once "includes/posts.include.php";
+
+// include admin options
+include_once "includes/options.include.php";
+
+include_once "widget.php";
+
+//get facebook feed
+function eff_get_page_feed(){
+    $defaults = array(
+      'facebook_page_id' => 'bbcnews',
+      'facebook_post_limit' => '5'
+    );
+
+    $options = get_option( 'eff_options', $defaults );
+    $url = "https://graph.facebook.com/".$options['facebook_page_id']."/posts?access_token=226916994002335|ks3AFvyAOckiTA1u_aDoI4HYuuw&limit=".$options['facebook_post_limit'];
+    $json = file_get_contents($url);
+    $feed = json_decode($json);
+
+    return $feed;
+}
+
+// get page information
+function eff_get_page(){
+    $defaults = array(
+      'facebook_page_id' => 'bbcnews',
+      'facebook_post_limit' => '5'
+    );
+
+    $options = get_option( 'eff_options', $defaults );
+    $url = "https://graph.facebook.com/".$options['facebook_page_id']."?access_token=226916994002335|ks3AFvyAOckiTA1u_aDoI4HYuuw";
+    $json = file_get_contents($url);
+    $page = json_decode($json);
+
+    return $page;
+}
+
+// add stylesheet
+function eff_stylesheet() {
+    wp_register_style( 'eff_bootstrap', plugins_url('css/eff_bootstrap.min.css?8', __FILE__) );
+    wp_register_style( 'eff_style', plugins_url('css/eff_style.css?8', __FILE__) );
+    wp_enqueue_style( 'eff_bootstrap' );
+    wp_enqueue_style( 'eff_style' );
+    wp_enqueue_style( 'eff-font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css', array(), '4.2.0' );
+}
+add_action( 'wp_enqueue_scripts', 'eff_stylesheet' );
+
+// add settings shortcut
+function plugin_add_settings_link( $links ) {
+    $settings_link = '<a href="options-general.php?page=easy-facebook-feed">' . __( 'Settings' ) . '</a>';
+    array_push( $links, $settings_link );
+    return $links;
+}
+$plugin = plugin_basename( __FILE__ );
+add_filter( "plugin_action_links_$plugin", 'plugin_add_settings_link' );
+
+function eff_easy_facebook_feed( $atts ){
+	$feed = eff_get_page_feed();
+    $page = eff_get_page();
+    $return = null;
+
+    foreach ($feed->data as $key => $data) {
+        switch ($data->type) {
+            case 'photo': 
+                $return .= eff_make_photo($data, $page);
+                break;
+            case 'link': 
+                $return .= eff_make_link($data, $page);
+                break;
+            case 'video': 
+                $return .= eff_make_video($data, $page);
+                break;
+            case 'status': 
+                $return .= eff_make_status($data, $page);
+                break;
+            default:
+                // skip unknown types
+                break;
+        }
+    }
+
+	return $return;
+}
+add_shortcode( 'easy_facebook_feed', 'eff_easy_facebook_feed' );
---
+++ readme.txt
@@ -0,0 +1,63 @@
+=== Easy facebook feed ===
+Contributors: timwass
+Author: Tim Wassenburg
+Donate link: http://stormware.nl
+Tags: facebook, feed
+Requires at least: 3.0.1
+Tested up to: 4.2.2
+Stable tag: 0.5
+Version: 0.5
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+With this plugin you can get your facebook posts on your wordpress website.
+
+== Description ==
+
+Get your facebook posts on your Wordpress website in a easy way. Features in Easy Facebook feed include:
+
+*   Displays shared links, video's, status updates and photo's from your Facebook page.
+*   Responsive layout.
+*	Uses the colors of your theme.
+*   Adjustable number of posts.
+*   Usable as full page.
+*   Usable as widget.
+*	Easy to use.
+
+== Installation ==
+
+1. Upload `easy-facebook-feed` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Go to the Easy Facebook settings and add your own Facebook ID
+1. Place `[easy_facebook_feed]` on your page
+
+And thats it, you are done! View the page to see the result.
+
+== Frequently Asked Questions ==
+
+= Where can I find my facebook id? =
+
+Your facebook id can be found in the url of your facebook page, for example: https://www.facebook.com/bbcnews, is this example 'bbcnews' is the facebook id.
+
+== Screenshots ==
+
+1. Example of how Easy Facebook feed looks in action.
+
+== Changelog ==
+
+= 0.5 =
+* Added widget support
+* Added settings shortcut on plugin page.
+
+= 0.4 =
+* Facebook made some graph changed causing errors in Easy Facebook Feed.
+
+= 0.3 =
+* Fixed a problem with the images because of a Facebook graph change. 
+* Layout bugfix in the link type post.
+
+= 0.2 =
+* Solved a small bug that occurred with older php versions.
+
+= 0.1 =
+* First release
